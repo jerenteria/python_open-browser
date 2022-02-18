2abd7848b45caf9c732c1789f3eecbdedb9d67eb
jerenteria <renteria.juan96@gmail.com>
2022-02-17 16:27:37 -0800

    something before i fully switch it up

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ from bs4 import BeautifulSoup
 ## url to open
 url = 'http://google.com/'
 
+## opens chrome
 chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
 webbrowser.get(chrome_path).open(url)
 
 result = requests.get(url)
 ## html.parser gets the actual html info from the page
 doc = BeautifulSoup(result.text, "html.parser")
-parent = search[0].parent
-search = parent.find(search)
 
+for search in doc.find_all()
